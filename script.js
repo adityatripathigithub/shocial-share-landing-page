64a9522bd21aa2fc191c212e88f54880584355f8
adity tripathi <tripathiaditya163@gmail.com>
2024-03-29 16:34:54 +0530

    Apply JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const box30 = document.querySelector(".box30")
+const heart = document.querySelector(".heart")
+const h2 = document.querySelector("h2")
+const close = document.querySelector("close")
+
+heart.addEventListener("click", function() {
+    heart.style.backgroundColor = "#29004b";
+    heart.style.color = "red";
+    heart.style.rotate = "100deg"
+})
+
+
+heart.addEventListener("click", function() {
+    h2.style.opacity = 1
+    h2.style.left = "18%"
+    
+})
